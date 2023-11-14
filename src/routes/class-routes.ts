9da99d16b462cdfd69853dcccc857d7c099fe2c2
ruan-cardozo <ruanvcardozo@gmail.com>
2023-11-13 21:12:31 -0300

    feat: instanciar Class

--- src/routes/class-routes.ts
+++ src/routes/class-routes.ts
@@ -2,9 +2,9 @@ import express from 'express'
 import { ClassController } from '../controllers/class-controller'
 
 const router = express.Router()
-const classController = new ClassController();
+const classController = new ClassController()
 
-router.get('/classes', classController.getAllClasses);
+router.get('/classes', classController.getAllClasses)
 router.post('/class/new', classController.createClass)
 router.put('/class/edit/:id_turma', classController.editClass)
 router.delete('/class/delete/:id_turma', classController.deleteClass)
